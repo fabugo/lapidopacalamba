4f3bc899b0aaf38c933a2f1b57fa1cd83fc9a8fb
Mathob <mathob.ce@outlook.com>
2016-02-21 19:11:01 -0300

    Alteração nas mensagens do teste do testador de flagas

--- sim/tb/tester_flags_tb.v
+++ sim/tb/tester_flags_tb.v
@@ -10,11 +10,11 @@ module tester_flags_tb;
 
 	initial begin
 		$display("\n---------------------------");
-		$display("Teste do Testador de Flags");
+		$display("Executando teste do Testador de Flags");
 		$display("Total de testes: 6");
 
 		OP_TF = 3'b111;
-		$display("Teste 1: (Nunca pula)");
+		$display("Executando teste 1: (Nunca pula)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -24,7 +24,7 @@ module tester_flags_tb;
 				C = i[2];
 				Z = i[3];
 				#1
-				if(out == 1'b0) $display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo nao era esperado.", O, S, C, Z, cond);
+				if(out == 1'b0) $display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo nao era esperado.", O, S, C, Z, cond);
 			end
 			case(cond)
 				3'b000: cond = 3'b001;
@@ -37,7 +37,7 @@ module tester_flags_tb;
 		end
 			
 		OP_TF = 3'b000;
-		$display("Teste 2: jf.COND (Pula se a condicao for falsa)");
+		$display("Executando teste 2: jf.COND (Pula se a condicao for falsa)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -59,7 +59,7 @@ module tester_flags_tb;
 						|| (cond == 4 && C == 0)
 						|| (cond == 5 && (S & Z) == 0)
 						|| (cond == 7 && O == 0)))
-					$display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
+					$display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
 
 			end
 			case(cond)
@@ -74,7 +74,7 @@ module tester_flags_tb;
 
 		OP_TF = 3'b001;
 			
-		$display("Teste 3: jt.COND (Pula se a condicao for verdadeira)");
+		$display("Executando teste 3: jt.COND (Pula se a condicao for verdadeira)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -89,14 +89,14 @@ module tester_flags_tb;
 						|| (cond == 4 && C == 0)
 						|| (cond == 5 && ~(S & Z) == 0)
 						|| (cond == 7 && O == 0)))
-					$display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo nao era esperado.", O, S, C, Z, cond);
+					$display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo nao era esperado.", O, S, C, Z, cond);
 				else if(out == 1'b1 && ((cond == 0)
 						|| (cond == 1 && S == 1)
 						|| (cond == 2 && Z == 1)
 						|| (cond == 4 && C == 1)
 						|| (cond == 5 && ~(S & Z) == 1)
 						|| (cond == 7 && O == 1)))
-					$display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
+					$display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
 			end
 			case(cond)
 				3'b000: cond = 3'b001;
@@ -109,7 +109,7 @@ module tester_flags_tb;
 		end
 
 		OP_TF = 3'b010;
-		$display("Teste 4: j L (Sempre pula)");
+		$display("Executando teste 4: j L (Sempre pula)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -119,7 +119,7 @@ module tester_flags_tb;
 				C = i[2];
 				Z = i[3];
 				#1
-				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
+				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
 			end
 			case(cond)
 				3'b000: cond = 3'b001;
@@ -132,7 +132,7 @@ module tester_flags_tb;
 		end
 
 		OP_TF = 3'b011;
-		$display("Teste 5: jal b (Sempre pula)");
+		$display("Executando teste 5: jal b (Sempre pula)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -142,7 +142,7 @@ module tester_flags_tb;
 				C = i[2];
 				Z = i[3];
 				#1
-				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
+				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
 			end
 			case(cond)
 				3'b000: cond = 3'b001;
@@ -155,7 +155,7 @@ module tester_flags_tb;
 		end
 
 		OP_TF = 3'b100;
-		$display("Teste 6: jr b (Sempre pula)");
+		$display("Executando teste 6: jr b (Sempre pula)");
 		exit = 0;
 		cond = 3'b000;
 		while(exit != 1) begin
@@ -165,7 +165,7 @@ module tester_flags_tb;
 				C = i[2];
 				Z = i[3];
 				#1
-				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b,  C: %b,  Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
+				if(out == 1'b1) $display("-> Erro para as entradas: O: %b, S: %b, C: %b, Z: %b, cond: %3b. O pulo esperado nao foi executado.", O, S, C, Z, cond);
 			end
 			case(cond)
 				3'b000: cond = 3'b001;
